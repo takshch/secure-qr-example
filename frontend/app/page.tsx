77b0265e09e018ad123f31238f2f68f90bd38237
takshch <takshch089@gmail.com>
2023-06-22 17:18:12 +0530

    Adds home page with approach buttons

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,21 @@
-import Image from 'next/image'
+import Link from 'next/link';
 
 export default function Home() {
-  return "Home Page"
+  return (
+    <div className="flex flex-col items-center my-6 mx-5">
+      <h1 className="text-2xl text-center mb-8">Choose approach by clicking</h1>
+      <div className="flex flex-col items-center gap-7">
+        <Link href="/user-key">
+          <button className="text-lg bg-purple-500 px-3 py-0.5 rounded text-white">
+            User Key Approach
+          </button>
+        </Link>
+        <Link href="/fixed-key">
+          <button className="text-lg bg-purple-500 px-3 py-0.5 rounded text-white">
+            Fixed Key Approach
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
 }
